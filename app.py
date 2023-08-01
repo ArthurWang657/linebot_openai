c34f7e3047d9e7ff50638ac7614086a3629e79a8
ArthurWang657 <66664756+ArthurWang657@users.noreply.github.com>
2023-08-01 22:24:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_completion_from_messages(messages, model = 'gpt-3.5-turbo'):
     )
     
     print(response.choices[0].message["content"])
-    return = response.choices[0].message["content"]
+    return response.choices[0].message["content"]
 
 
 
